18ed93e1caf50ccb708b42d3b2fc96580961d8cd
Varun Goel <babyvarun96@gmail.com>
2016-02-09 19:43:23 -0500

    First Commit

---
+++ Yelp/BusinessCell.swift
@@ -0,0 +1,24 @@
+//
+//  BusinessCell.swift
+//  Yelp
+//
+//  Created by Varun Goel on 2/9/16.
+//  Copyright © 2016 Timothy Lee. All rights reserved.
+//
+
+import UIKit
+
+class BusinessCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
